fd742e46e6376182c752a78ad39cf72a7626fb69
Oms-343 <oms87473@gmail.com>
2024-01-17 22:13:07 +0530

    booting up project template

---
+++ src/data.js
@@ -0,0 +1,9 @@
+// initial data
+export const cardImages = [
+  { src: "/app-images/apple.png", matched: false },
+  { src: "/app-images/microsoft.jpeg", matched: false },
+  { src: "/app-images/facebook.png", matched: false },
+  { src: "/app-images/amazon.png", matched: false },
+  { src: "/app-images/netflix.png", matched: false },
+  { src: "/app-images/google.png", matched: false },
+];
