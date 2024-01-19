a942192f9fe29a660f81adba83753fa7e07bcc16
Oms-343 <oms87473@gmail.com>
2024-01-19 18:03:37 +0530

    starter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
+
 import { cardImages } from "./data";
 
 function App() {
+  const [cards, setCards] = useState([]);
+
   function handleGenerateCards() {
     const cardsFullLength = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
 
-    console.log(cardsFullLength);
+    setCards(cardsFullLength);
   }
 
   return (
-    <div>
+    <div className="App">
       <h1>Pair Up</h1>
       <button onClick={handleGenerateCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card-img" />
+              <img src="/app-images/cover.png" alt="cover-img" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
