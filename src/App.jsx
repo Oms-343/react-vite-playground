00e89c7747b6978595cd2bc1cbc8a2cc9209c4cb
Oms-343 <oms87473@gmail.com>
2024-01-26 21:15:39 +0530

    starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 function App() {
-  function handleGenerateCards() {}
-
-  return (
-    <div>
-      <h1>Pair Up</h1>
-      <button onClick={handleGenerateCards}>New Game</button>
-    </div>
-  );
+  return <div>Hello React</div>;
 }
 
 export default App;
--- src/data.js
+++
@@ -1,9 +0,0 @@
-// initial data
-export const cardImages = [
-  { src: "/app-images/apple.png", matched: false },
-  { src: "/app-images/microsoft.jpeg", matched: false },
-  { src: "/app-images/facebook.png", matched: false },
-  { src: "/app-images/amazon.png", matched: false },
-  { src: "/app-images/netflix.png", matched: false },
-  { src: "/app-images/google.png", matched: false },
-];
